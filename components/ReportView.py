b1c838cad5131925c1992ff2940afb090baba7ea
yokat <84916937+yokat@users.noreply.github.com>
2023-09-20 11:39:19 +0200

    including data

--- components/ReportView.py
+++ components/ReportView.py
@@ -1,18 +1,26 @@
 import plotly.express as px
 
-def Reports(df, dates):
+def Reports(sub_bahis_sourcedata, dates):
+
+    periodClick=1
     tmp = sub_bahis_sourcedata["date"].dt.date.value_counts()
     tmp = tmp.to_frame()
     tmp["counts"] = tmp["date"]
     tmp["date"] = pd.to_datetime(tmp.index)
-    tmp = tmp["counts"].groupby(tmp["date"].dt.to_period("W-SAT")).sum().astype(int)
+    if periodClick== 3:
+        tmp=tmp['counts'].groupby(tmp['date']).sum().astype(int)
+    if periodClick== 2:
+        tmp=tmp['counts'].groupby(tmp['date'].dt.to_period('W-SAT')).sum().astype(int)
+    if periodClick== 1:
+        tmp=tmp['counts'].groupby(tmp['date'].dt.to_period('M')).sum().astype(int)
     tmp = tmp.to_frame()
     tmp["date"] = tmp.index
     tmp["date"] = tmp["date"].astype("datetime64[D]")
+
     fig = px.bar(tmp, x="date", y="counts", labels={"date": "", "counts": "No. of Reports"})
     
-    print(df)
     df = px.data.gapminder()
+    print(df)
     fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
             size="pop", color="continent", hover_name="country",
             log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
