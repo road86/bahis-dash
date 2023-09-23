248a5e428940fc253ead450a27501efd40961e86
yokat <84916937+yokat@users.noreply.github.com>
2023-09-23 22:45:54 +0200

    Changed Monthnames

--- components/yearly_comparison.py
+++ components/yearly_comparison.py
@@ -3,18 +3,26 @@ import plotly.express as px
 
 def yearlyComp(bahis_data):
     monthly = bahis_data.groupby(
-        [bahis_data["date"].dt.year.rename("year"), bahis_data["date"].dt.month.rename("month")]
+        [bahis_data["date"].dt.year.rename("Year"), bahis_data["date"].dt.month.rename("Month")]
     )["date"].agg({"count"})
     monthly = monthly.rename({"count": "reports"}, axis=1)
     monthly = monthly.reset_index()
-    monthly["year"] = monthly["year"].astype(str)
+    monthly["Year"] = monthly["Year"].astype(str)
     figYearlyComp = px.bar(
         data_frame=monthly,
-        x="month",
+        x="Month",
         y="reports",
-        labels={"month": "", "reports": "Reports"},
-        color="year",
+        labels={"Month": "Month", "reports": "Reports"},
+        color="Year",
         barmode="group",
     )
-    figYearlyComp.update_xaxes(dtick="M1", tickformat="%B \n%Y")
+    figYearlyComp.update_xaxes(dtick="M1")  # , tickformat="%B \n%Y")
+    figYearlyComp.update_layout(
+        xaxis=dict(
+            tickmode='array',
+            tickvals=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            ticktext=['January', 'February', 'March', 'April', 'May', 'June',
+                      'July', 'August', 'September', 'October', 'November', 'December']
+        )
+    )
     return figYearlyComp
