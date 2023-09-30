977f3b287a1e4008436e6661d93feb65e01d40e6
yokat <84916937+yokat@users.noreply.github.com>
2023-09-30 16:35:20 +0200

    Prep for Navbar alignment not perfect yet

--- components/navbar.py
+++ components/navbar.py
@@ -26,39 +26,72 @@ create_date = fetchdata.create_date(sourcefilename)
 def Navbar():
     img_logo = "assets/Logo.png"
 
-    surf = dbc.Navbar(
-        dbc.Container(
+    withlinks = True
+
+    if withlinks:
+        surf = dbc.Navbar(
+            dbc.Container(
+                [
+                    # dcc.Location(id='urloc', refresh=True),
+                    dbc.Nav(
+                        [
+                            dbc.NavLink(
+                                [
+                                    html.Div(page["name"], className="ms-2"),
+                                ],
+                                href=page["path"],
+                                active="exact",
+                            )
+                            for page in dash.page_registry.values()
+                            # if page["path"].startswith("/topic")
+                        ]
+                    ),
+                    dbc.Row(
+                        [
+                            dbc.Col(
+                                [
+                                    # dbc.NavbarBrand("BAHIS Dashboard", className="ml-auto")
+                                    html.Label("BAHIS dashboard", style={"font-weight": "bold",
+                                                                         "font-size": "200%", "text-align": "center"}),
+                                ],
+                                width=9
+                            ),
+                            dbc.Col(
+                                [
+                                    html.Img(src=img_logo, height="30px")
+                                ],
+                                width=3),
+                        ],
+                        justify="left",
+                        align="center"
+                    ),
+
+                ],
+                # fluid=True,
+            ),
+            color="white",
+            dark=False,
+            className="mb-5",
+        )
+    else:
+        surf = html.Div(
             [
-                # dcc.Location(id='urloc', refresh=True),
                 dbc.Row(
                     [
-                        dbc.Col(html.Img(src=img_logo, height="30px")),
                         dbc.Col(
-                            #dbc.NavbarBrand("BAHIS Dashboard", className="ml-auto")
-                            html.Label("BAHIS dashboard", style={"font-weight": "bold", "font-size": "200%", "text-align": "center"})
+                            html.Label("BAHIS dashboard", style={"font-weight": "bold",
+                                                                 "font-size": "200%"}),  # , "text-align": "center"}),
+                            width=5,
                         ),
-                    ],
-                    align="center",
-                ),
-                dbc.Nav(
-                    [
-                        dbc.NavLink(
-                            [
-                                html.Div(page["name"], className="ms-2"),
-                            ],
-                            href=page["path"],
-                            active="exact",
+                        dbc.Col(
+                            html.Img(src=img_logo, height="30px"),
+                            width=3,
+                            # align='right'
                         )
-                        for page in dash.page_registry.values()
-                        # if page["path"].startswith("/topic")
-                    ]
-                ),
-            ],
-            fluid=True,
-        ),
-        color="white",
-        dark=False,
-        className="mb-5",
-    )
-
+                    ],
+                    justify="end",
+                    align="center"
+                )
+            ]
+        )
     return surf
