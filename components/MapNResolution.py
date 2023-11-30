e7c0e8e037f2d3bdb2e6214d8aa820b3ea5cc64b
yokat <84916937+yokat@users.noreply.github.com>
2023-11-30 15:56:10 +0100

    stuck with division map not working

--- components/MapNResolution.py
+++ components/MapNResolution.py
@@ -9,56 +9,46 @@ def open_data(path):
         data = json.load(f)
     return data
 
-def plot_map(path, loc, subDistM, sub_bahis_sourcedata, title, pnumber, pname, splace, variab, labl):
-    reports = sub_bahis_sourcedata[title].value_counts().to_frame()
-    reports[pnumber] = reports.index  # 1
-    reports.index = reports.index.astype(int)  # upazila name
-    reports[pnumber] = reports[pnumber].astype(int)
-    reports = reports.loc[
-        reports[pnumber] != "nan"
-    ]  # unknown reason for now. does this have to be beore reports in sub_bahis_sourcedata?
-    # reports, where there are no geonumbers?
-    data = open_data(path)  # 1
-    reports[pname] = reports.index
-    tmp = subDistM[["value", "name"]]
-    tmp = tmp.rename(columns={"value": pnumber, "name": pname})
-    tmp[pname] = tmp[pname].str.title()
-    tmp["Index"] = tmp[pnumber]
-    tmp = tmp.set_index("Index")
-    tmp[title] = 0
+def plotMap(geoResolution, geoResolutionNo, reportsdata, geoNameNNumber, shapePath):
+        
+    reports = reportsdata[geoResolution].value_counts().to_frame()
+    reports[geoResolution + "number"] = reports.index  #   print(reports.loc[reports[geoResolution + "number"] == "nan"])  reports, with no geonumbers?
+    reports = reports.rename(columns = {geoResolution: "cases"})
 
-    aaa = reports.combine_first(tmp)
-    aaa[pname] = tmp[pname]
-    del tmp
-    del reports
-    reports = aaa
-    for i in range(reports.shape[0]):  # go through all upazila report values
-        reports[pname].iloc[i] = subDistM[subDistM["value"] == reports.index[i]]["name"].values[
-            0
-        ]  # still to work with the copy , this goes with numbers and nnot names
-    reports[pname] = reports[pname].str.title()
-    reports.set_index(pnumber)  # 1
+    geoNameNNumber= geoNameNNumber[geoNameNNumber["loc_type"]==geoResolutionNo]
+    geoNameNNumber=geoNameNNumber.rename(columns = {"value": geoResolution + "number", "name": geoResolution + "name"})[[geoResolution + "number", geoResolution + "name"]]
+    geoNameNNumber= geoNameNNumber.set_index(geoResolution + "number")
+    geoNameNNumber[geoResolution + "name"] = geoNameNNumber[geoResolution + "name"].str.title()
 
-    custolor = [[0, "white"], [1 / reports[title].max(), "lightgray"], [1, "red"]]
+    reports = reports.combine_first(geoNameNNumber)
+
+    shapedata = open_data(shapePath)  
+    if geoResolutionNo == 1:
+        reports["divisionname"] = reports["divisionname"] + " Division"
+        print(reports)
+    if geoResolutionNo == 2:
+        print(reports)
+
+    custolor = [[0, "white"], [1 / reports["cases"].max(), "lightgray"], [1, "red"]]
     fig = px.choropleth_mapbox(
         reports,
-        geojson=data,
-        locations=pnumber,
-        color=title,
-        featureidkey="properties." + pnumber,
+        geojson=shapedata,
+        locations=geoResolution + "number",
+        color="cases",
+        featureidkey="properties." + geoResolution + "number",
         color_continuous_scale=custolor,
-        range_color=(1, reports[title].max()),
+        range_color=(1, reports["cases"].max()),
         mapbox_style="carto-positron",
         zoom=5.8,
         center={"lat": 23.7, "lon": 90.3},
         opacity=0.7,
-        labels={variab: labl},
-        hover_name=pname,
-        hover_data={pname: False, pnumber: False}
+        labels={"cases": "Reports"},
+        hover_name=geoResolution + "name" ,
+        hover_data={geoResolution + "name" : False, geoResolution + "number": False}
     )
     fig.update_layout(
-        autosize=True, coloraxis_showscale=True, margin={"r": 0, "t": 0, "l": 0, "b": 0}, height=550
-    )  # , width=760 , height=800, ) #, coloraxis_showscale= False) #width= 1000, height=600,
+        autosize=True, coloraxis_showscale=True, margin={"r": 0, "t": 0, "l": 0, "b": 0}, height=550)
+    
     return fig
 
 
@@ -82,29 +72,29 @@ Form = html.Div([
 
 
 @callback(
-    Output("Map", "figure"),
+    Output("Map", "figure", allow_duplicate=True),
     Input("cache_bahis_data", "data"),
     Input("cache_bahis_geodata", "data"),
+    prevent_initial_call=True
 )
 
-def plotMap(sourcedata, geodata):
+def mapPrep(sourcedata, geodata):
 
-    sub_bahis_sourcedata=pd.read_json(sourcedata, orient="split")
-    subDist=pd.read_json(geodata, orient="split")
-    path = "exported_data/processed_geodata/upadata.geojson"
-    loc = 3
-    title = "upazila"
-    pnumber = "upazilanumber"
-    pname = "upazilaname"
-    splace = " Upazila"
-    variab = "upazila"
-    subDistM = subDist[subDist["loc_type"] == 3]
-    labl = "Reports"
-    # subDist=bahis_geodata[bahis_geodata['loc_type']==geoSlider]
+    geoResolution="upazila"
+    geoResolutionNo=3
+    reportsdata=pd.read_json(sourcedata, orient="split")
+    geoNameNNumber=pd.read_json(geodata, orient="split")
+    shapePath = "exported_data/processed_geodata/upadata.geojson"
+    fig = plotMap(geoResolution, geoResolutionNo, reportsdata, geoNameNNumber, shapePath)
+    return fig
 
-#    if ctx.triggered_id == 'geoSlider':
-#        Rfindic, Rfigg, NRlabel, AlertTable = GeoRep.GeoRep(sub_bahis_sourcedata, title,
-#                                                            subDistM, pnumber, pname, variab, labl)
+#     # subDist=bahis_geodata[bahis_geodata['loc_type']==geoSlider]
 
-    Rfig = plot_map(path, loc, subDistM, sub_bahis_sourcedata, title, pnumber, pname, splace, variab, labl)
-    return Rfig
+# #    if ctx.triggered_id == 'geoSlider':
+# #        Rfindic, Rfigg, NRlabel, AlertTable = GeoRep.GeoRep(sub_bahis_sourcedata, title,
+# #                                                            subDistM, pnumber, pname, variab, labl)
+
+# #    Rfig = plot_map(path, subDistM, sub_bahis_sourcedata, title, pnumber, pname, variab, labl)
+#     Rfig = plot_map(path, subDistM, reportsdata, title, pnumber, pname, variab, labl)
+#     return Rfig
+
--- pages/tst.py
+++ pages/tst.py
@@ -8,61 +8,6 @@ import pandas as pd
 
 dash.register_page(__name__,)  # register page to main dash app
 
-# def plot_map(path, loc, subDistM, sub_bahis_sourcedata, title, pnumber, pname, splace, variab, labl):
-#     reports = sub_bahis_sourcedata[title].value_counts().to_frame()
-#     reports[pnumber] = reports.index  # 1
-#     reports.index = reports.index.astype(int)  # upazila name
-#     reports[pnumber] = reports[pnumber].astype(int)
-#     reports = reports.loc[
-#         reports[pnumber] != "nan"
-#     ]  # unknown reason for now. does this have to be beore reports in sub_bahis_sourcedata?
-#     # reports, where there are no geonumbers?
-#     data = open_data(path)  # 1
-#     reports[pname] = reports.index
-#     tmp = subDistM[["value", "name"]]
-#     tmp = tmp.rename(columns={"value": pnumber, "name": pname})
-#     tmp[pname] = tmp[pname].str.title()
-#     tmp["Index"] = tmp[pnumber]
-#     tmp = tmp.set_index("Index")
-#     tmp[title] = 0
-
-#     aaa = reports.combine_first(tmp)
-#     aaa[pname] = tmp[pname]
-#     del tmp
-#     del reports
-#     reports = aaa
-#     for i in range(reports.shape[0]):  # go through all upazila report values
-#         reports[pname].iloc[i] = subDistM[subDistM["value"] == reports.index[i]]["name"].values[
-#             0
-#         ]  # still to work with the copy , this goes with numbers and nnot names
-#     reports[pname] = reports[pname].str.title()
-#     reports.set_index(pnumber)  # 1
-
-#     custolor = [[0, "white"], [1 / reports[title].max(), "lightgray"], [1, "red"]]
-#     fig = px.choropleth_mapbox(
-#         reports,
-#         geojson=data,
-#         locations=pnumber,
-#         color=title,
-#         featureidkey="properties." + pnumber,
-#         color_continuous_scale=custolor,
-#         range_color=(1, reports[title].max()),
-#         mapbox_style="carto-positron",
-#         zoom=5.8,
-#         center={"lat": 23.7, "lon": 90.3},
-#         opacity=0.7,
-#         labels={variab: labl},
-#         hover_name=pname,
-#         hover_data={pname: False, pnumber: False}
-#     )
-#     fig.update_layout(
-#         autosize=True, coloraxis_showscale=True, margin={"r": 0, "t": 0, "l": 0, "b": 0}, height=550
-#     )  # , width=760 , height=800, ) #, coloraxis_showscale= False) #width= 1000, height=600,
-#     return fig
-
-
-
-
 layout = html.Div([
     # dcc.Store(id="cache_bahis_data", storage_type="memory"),
     # dcc.Store(id="cache_bahis_dgdata", storage_type="memory"),
@@ -88,16 +33,19 @@ layout = html.Div([
 @callback(
     Output("District", "options"),
     Output("Upazila", "options"),
+    Output("Map", "figure"),
     Input("Division", "value"),
     Input("District", "value"),
     Input("Upazila", "value"),
     Input("District", "options"),
     Input("Upazila", "options"),
+    Input("geoSlider", "value"),
+    Input("cache_bahis_data", "data"),
     Input("cache_bahis_geodata", "data"),
     prevent_initial_call=True
 )
 
-def RegionSelect(SelectedDivision, SelectedDistrict, SelectedUpazila, DistrictList, UpazilaList, geodata):
+def RegionSelect(SelectedDivision, SelectedDistrict, SelectedUpazila, DistrictList, UpazilaList, geoSlider, sourcedata, geodata):
     bahis_geodata=pd.read_json(geodata, orient="split")
     if DistrictList == None: DistrictList = []
     if UpazilaList == None: UpazilaList = []
@@ -135,45 +83,31 @@ def RegionSelect(SelectedDivision, SelectedDistrict, SelectedUpazila, DistrictLi
 #            subDist = bahis_geodata.loc[bahis_geodata["value"].astype("string").str.startswith(str(SelectedUpazila ))]
 #            sub_bahis_sourcedata = bahis_data.loc[bahis_data["upazila"] == SelectedUpazila ]
 
- if geoSlider == 1:
-        path = path1
-        loc = geoSlider
-        title = "division"
-        pnumber = "divnumber"
-        pname = "divisionname"
-        splace = " Division"
-        variab = "division"
-        subDistM = subDist[subDist["loc_type"] == geoSlider]
+    if geoSlider == 1:
+        geoResolution = "division"
+        reportsdata = pd.read_json(sourcedata, orient="split")
+        geoNameNNumber = pd.read_json(geodata, orient="split")
+        shapePath = "exported_data/processed_geodata/divdata.geojson"
+        Mapfig = MapNResolution.plotMap(geoResolution, geoSlider, reportsdata, geoNameNNumber, shapePath)
 
     if geoSlider == 2:
-        path = path2
-        loc = geoSlider
-        title = "district"
-        pnumber = "districtnumber"
-        pname = "districtname"
-        splace = " District"
-        variab = "district"
-        subDistM = subDist[subDist["loc_type"] == geoSlider]
-        # subDist=bahis_geodata[bahis_geodata['loc_type']==geoSlider]
+        geoResolution = "district"
+        reportsdata = pd.read_json(sourcedata, orient="split")
+        geoNameNNumber = pd.read_json(geodata, orient="split")
+        shapePath = "exported_data/processed_geodata/distdata.geojson"
+        Mapfig = MapNResolution.plotMap(geoResolution, geoSlider, reportsdata, geoNameNNumber, shapePath)
 
     if geoSlider == 3:
-        path = path3
-        loc = geoSlider
-        loc = 3
-        title = "upazila"
-        pnumber = "upazilanumber"
-        pname = "upazilaname"
-        splace = " Upazila"
-        variab = "upazila"
-        subDistM = subDist[subDist["loc_type"] == 3]
-        # subDist=bahis_geodata[bahis_geodata['loc_type']==geoSlider]
+        geoResolution = "upazila"
+        reportsdata = pd.read_json(sourcedata, orient="split")
+        geoNameNNumber = pd.read_json(geodata, orient="split")
+        shapePath = "exported_data/processed_geodata/upadata.geojson"
+        Mapfig = MapNResolution.plotMap(geoResolution, geoSlider, reportsdata, geoNameNNumber, shapePath)
 
-    if ctx.triggered_id == 'geoSlider':
-        Rfindic, Rfigg, NRlabel, AlertTable = GeoRep.GeoRep(sub_bahis_sourcedata, title,
-                                                            subDistM, pnumber, pname, variab, labl)
+    # if ctx.triggered_id == 'geoSlider':
+    #     Rfindic, Rfigg, NRlabel, AlertTable = GeoRep.GeoRep(sub_bahis_sourcedata, title,
+    #                                                         subDistM, pnumber, pname, variab, labl)
 
-    Rfig = plot_map(path, loc, subDistM, sub_bahis_sourcedata, title, pnumber, pname, splace, variab, labl)
+    # Rfig = plot_map(path, subDistM, sub_bahis_sourcedata, title, pnumber, pname, variab, labl)
 
-
-
-    return DistrictList, UpazilaList
+    return DistrictList, UpazilaList, Mapfig
