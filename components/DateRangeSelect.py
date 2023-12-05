f9ed559d7a20bb4c64dc999d8a5c6bba4189bec3
yokat <84916937+yokat@users.noreply.github.com>
2023-12-05 15:04:03 +0100

    1st step completeness. interactions not wired

--- components/DateRangeSelect.py
+++ components/DateRangeSelect.py
@@ -1,31 +1,35 @@
+from datetime import timedelta, date
 from components import fetchdata
-import dash_bootstrap_components as dbc
 from dash import html, dcc, callback
 from dash.dependencies import Input, Output
 import pandas as pd
 
+
+start_date = date(2019, 1, 1)
+last_date = date(2023, 1, 1)  #max(bahis_data['date']).date()
+# fetchdata.create_date(sourcefilename)
 
 Form = html.Div([
     dcc.DatePickerRange(
         id="daterange",
         min_date_allowed=start_date,
         start_date=last_date - timedelta(weeks=6),  # date(2023, 1, 1),
-        max_date_allowed=create_date,
-        end_date=last_date,  # date(2023, 12, 31)
+        max_date_allowed = last_date,   # create_date,
+        end_date = last_date,  # date(2023, 12, 31)
     )
 ])
 
-@callback(
-    Output("Diseaselist", "options", allow_duplicate=True),
-    Input("cache_bahis_data", "data"),
-    prevent_initial_call=True
-)
+# @callback(
+#     Output("daterange", "options", allow_duplicate=True),
+#     Input("cache_bahis_data", "data"),
+#     prevent_initial_call=True
+# )
 
-def DiseaseList(bahis_data):
-    List = fetchdata.fetchDiseaselist(pd.read_json(bahis_data, orient="split"))
-    print("aa")
-    print(List)
-    print("here")
-    Diseaselist = [{"label": i["Disease"], "value": i["value"]} for i in List]
-    print(Diseaselist)
-    return Diseaselist
+# def Dates(bahis_data):
+#     # List = fetchdata.fetchDiseaselist(pd.read_json(bahis_data, orient="split"))
+#     min = pd.read_json(bahis_data, orient="split")['date'].min()
+#     max = pd.read_json(bahis_data, orient="split")['date'].max()
+#     print(min)
+#     print(max)
+
+#     return out
