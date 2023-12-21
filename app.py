a25dff5d5a8c36d7a99f896d51d5bee78a8131a6
yokat <84916937+yokat@users.noreply.github.com>
2023-12-21 11:59:15 +0100

    Step 1 roughly working

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import dash_bootstrap_components as dbc
 from components import navbar, pathnames, fetchdata
 from dash import Dash, Input, Output, dcc, html, State
 
+from components import fetchdata, RegionSelect, MapNResolution, DateRangeSelect, DiseaseSelect # , CompletenessReport
+import pandas as pd
+
+
 app = Dash(
     __name__,
     use_pages=True,
@@ -32,7 +36,60 @@ app.layout = html.Div(
         navbar.Navbar(),
 #        html.Div(id="page-1-display-value"),
         html.Br(),
-        dash.page_container,
+
+        dbc.Row(
+            [
+                dbc.Col(            # left side
+                    [
+                        dbc.Card(
+                            dbc.CardBody(RegionSelect.Form),
+                        ),
+                        dbc.Card(
+                            dbc.CardBody(MapNResolution.Form)
+                        )
+                    ],
+                    width=4,
+                ),
+                dbc.Col([          # right side
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                dbc.Row(
+                                    [
+                                        dbc.Col(
+                                            DateRangeSelect.Form
+                                        ),
+                                        dbc.Col(
+                                            DiseaseSelect.Form
+                                        )
+                                    ]
+                                )
+                            ]
+                        )
+                    ),
+                    dbc.Card(
+                        dbc.CardBody(
+                            [dbc.Card(
+                                dbc.CardBody(
+                                    # [
+                                    dash.page_container,
+                                    # [
+                                    #     html.Label("Weekly Completeness"),
+                                    #     dbc.Col(
+                                    #         [
+                                    #             dcc.Graph(id="Completeness")
+                                    #         ]
+                                    #     )
+                                    # ]
+                                )
+                            )]
+                        ),
+                    ),
+                ])
+            ]
+        ),
+        
+#        dash.page_container,
         html.Br(),
         html.Div(id="dummy"),
         html.Label('Data from ' + str(create_date), style={'text-align': 'right'}),
@@ -40,6 +97,7 @@ app.layout = html.Div(
         dcc.Store(data=bahis_dgdata.to_json(date_format='iso', orient='split'), id="cache_bahis_dgdata", storage_type="memory"),
         dcc.Store(data=bahis_distypes.to_json(date_format='iso', orient='split'), id="cache_bahis_distypes", storage_type="memory"),
         dcc.Store(id="cache_bahis_geodata", storage_type="memory"),
+        dcc.Store(id="cache_page_settings", storage_type="memory"),
     ]
 )
 
@@ -72,3 +130,123 @@ if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
 else:
     server = app.server
+
+
+@app.callback(
+    Output("District", "options", allow_duplicate=True),
+    Output("Upazila", "options", allow_duplicate=True),
+    Output("Map", "figure", allow_duplicate=True),
+    Output("cache_page_settings", "data"),    
+#    Output("Completeness", "figure"),
+    Input("Division", "value"),
+    Input("District", "value"),
+    Input("Upazila", "value"),
+    Input("District", "options"),
+    Input("Upazila", "options"),
+    Input("geoSlider", "value"),
+    Input("DateRange", "value"),
+    Input("Disease", "value"),
+#    State("Completeness", "figure"),
+    State("cache_bahis_data", "data"),
+    State("cache_bahis_geodata", "data"),
+    State("cache_bahis_geodata", "data"),
+    prevent_initial_call=True
+)
+
+def Framework(SelectedDivision, SelectedDistrict, SelectedUpazila, DistrictList, UpazilaList, geoSlider, DateRange, SelectedDisease, sourcedata, geodata): # CompletenessFig, sourcedata, geodata):
+
+    reportsdata = pd.read_json(sourcedata, orient="split")
+    geoNameNNumber = pd.read_json(geodata, orient="split")
+    geoResolution = "upazila"
+    shapePath = "exported_data/processed_geodata/upadata.geojson"       # change to relative path names later further 3 instances
+
+    reportsdata = fetchdata.date_subset(DateRange, reportsdata)
+    reportsdata = fetchdata.disease_subset(SelectedDisease, reportsdata)
+
+    if DistrictList is None: 
+        DistrictList = []
+    if UpazilaList is None: 
+        UpazilaList = []
+
+    if ctx.triggered_id == "Division":
+        if not SelectedDivision:
+            DistrictList = []
+            UpazilaList = []
+        else:
+            reportsdata = reportsdata.loc[reportsdata["division"] == SelectedDivision]
+            geoNameNNumber = geoNameNNumber.loc[geoNameNNumber["parent"].astype("string").str.startswith(str(SelectedDivision))]
+            List = fetchdata.fetchDistrictlist(SelectedDivision, geoNameNNumber)
+            DistrictList = [{"label": i["District"], "value": i["value"]} for i in List]
+            UpazilaList = []
+    
+    if ctx.triggered_id == "District":
+        if not SelectedDistrict:
+            reportsdata = reportsdata.loc[reportsdata["division"] == SelectedDivision]
+            geoNameNNumber = geoNameNNumber.loc[geoNameNNumber["parent"].astype("string").str.startswith(str(SelectedDivision))]
+            UpazilaList = []
+        else:
+            reportsdata = reportsdata.loc[reportsdata["district"] == SelectedDistrict]
+            geoNameNNumber = geoNameNNumber.loc[geoNameNNumber["parent"].astype("string").str.startswith(str(SelectedDistrict))]
+            List = fetchdata.fetchUpazilalist(SelectedDistrict, geoNameNNumber)
+            UpazilaList = [{"label": i["Upazila"], "value": i["value"]} for i in List]
+
+    if ctx.triggered_id == "Upazila":
+        if not SelectedUpazila:
+            reportsdata = reportsdata.loc[reportsdata["district"] == SelectedDistrict]
+            geoNameNNumber = geoNameNNumber.loc[geoNameNNumber["parent"].astype("string").str.startswith(str(SelectedDistrict))]
+        else:
+            reportsdata = reportsdata.loc[reportsdata["upazila"] == SelectedUpazila]  
+            geoNameNNumber = geoNameNNumber.loc[geoNameNNumber["value"] == SelectedUpazila]     # check if valid for other "tabs"
+
+    if geoSlider == 1:
+        geoResolution = "division"
+        shapePath = "exported_data/processed_geodata/divdata.geojson"           # keep in mind to adjust
+    
+    if geoSlider == 2:
+        geoResolution = "district"
+        shapePath = "exported_data/processed_geodata/distdata.geojson"
+    
+    if geoSlider == 3:
+        geoResolution = "upazila"
+        shapePath = "exported_data/processed_geodata/upadata.geojson"
+    
+    # if UpazilaList =  []
+    MapFig = MapNResolution.plotMap(geoResolution, geoSlider, reportsdata, geoNameNNumber, shapePath)
+
+    # if ctx.triggered_id == 'geoSlider':
+    #     Rfindic, Rfigg, NRlabel, AlertTable = GeoRep.GeoRep(sub_bahis_sourcedata, title,
+    #                                                         subDistM, pnumber, pname, variab, labl)
+
+    # Rfig = plot_map(path, subDistM, sub_bahis_sourcedata, title, pnumber, pname, variab, labl)
+    
+    # if SelectedUpazila==None:
+    #     CompletenessFig = CompletenessReport.generate_reports_heatmap(reportsdata,
+    #         geoNameNNumber, DateRange[0], DateRange[1], SelectedDivision, SelectedDistrict)
+    # else:
+    #     CompletenessFig = CompletenessFig
+    
+    if SelectedUpazila != None:
+        UpazilaEntry = SelectedUpazila
+    else:
+        UpazilaEntry= UpazilaList
+        if SelectedDistrict != None:
+            DistrictEntry = SelectedDistrict
+        else:
+            DistrictEntry = DistrictList
+            if SelectedDivision != None:
+                DivisionEntry = SelectedDivision
+            else:
+                DivisionEntry = DivisionList
+
+
+    page_settings = {  
+        "division": DivisionEntry,
+        "district": DistrictEntry,
+        "upazila": DistrictEntry,
+        "disease": SelectedDisease,
+        "daterange": DateRange
+    }
+
+    print(page_settings)
+
+    return DistrictList, UpazilaList, MapFig, page_settings.to_json(date_format='iso', orient='split') # , CompletenessFig
