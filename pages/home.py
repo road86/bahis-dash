2cfa347b72975c40cbb87da50fdac77fe5731bd9
yokat <84916937+yokat@users.noreply.github.com>
2024-01-08 09:00:09 +0100

    homepagelink

--- pages/home.py
+++ pages/home.py
@@ -1,12 +1,9 @@
-from dash import Dash, html, dcc
+from dash import html
 import dash
 
-dash.register_page(__name__,)
+dash.register_page(__name__, path='/')
 
-layout = html.Div(
-    [html.H2("Welcome to the bahis dashboard."),
-     html.H2("Please select a report from the menu on the top left.")
-    ])
-
-if __name__ == '__main__':
-    app.run(debug=True)
+layout = html.Div([
+    html.H2("Welcome to the bahis dashboard."),
+    html.H2("Please select a report from the menu on the top left.")
+])
