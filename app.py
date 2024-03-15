1c3efba8bbe64fa1f40f0d46dc91a6b3e5bfc82a
yokat <84916937+yokat@users.noreply.github.com>
2024-03-15 17:49:25 +0100

    update topic again fixed

--- app.py
+++ app.py
@@ -221,6 +221,7 @@ def display_valueNtoggle_offcanvas(n1, is_open):
     Output("sidemenu", "is_open", allow_duplicate=True),
     Output("Disease", "options", allow_duplicate=True),
     Output("cache_urlorigin", "data"),
+    Output("dummy", "id", allow_duplicate=True),
     Input("Map", "figure"),
     Input("_pages_location", "href"),
     Input("cache_page_data", "data"),
@@ -228,9 +229,10 @@ def display_valueNtoggle_offcanvas(n1, is_open):
     Input("cache_page_geodata", "data"),
     Input("cache_page_settings", "data"),
     Input("cache_urlorigin", "data"),
+    Input("dummy", "id"),
     prevent_initial_call=True,
 )
-def sideandmap(MapFig, urlnext, data, farmdata, geodata, settings, urlorigin):
+def sideandmap(MapFig, urlnext, data, farmdata, geodata, settings, urlorigin, dummy):
     first = urlnext.find("/")
     f = 3
     while first >= 0 and f > 1:
@@ -268,6 +270,7 @@ def sideandmap(MapFig, urlnext, data, farmdata, geodata, settings, urlorigin):
                 False,
                 DiseaseList,
                 urlnext,
+                dummy,
             )
     else:
         if (not urlorigin or urlorigin.startswith("fa")) and data is not None:
@@ -280,9 +283,10 @@ def sideandmap(MapFig, urlnext, data, farmdata, geodata, settings, urlorigin):
                 False,
                 DiseaseList,
                 urlnext,
+                dummy,
             )
 
-    return MapFig, False, DiseaseList, urlnext
+    return MapFig, False, DiseaseList, urlnext, dummy
 
 
 @app.callback(
