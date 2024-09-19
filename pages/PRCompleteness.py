414b28f67606dc85f6c00e9b7a2f3e48604dda38
Sharful Islam <34204530+shaarful@users.noreply.github.com>
2024-09-19 11:27:13 +0600

    Feat change color for completeness (#354)
    * feat: change color
    
    * feat: more color adjust

--- pages/PRCompleteness.py
+++ pages/PRCompleteness.py
@@ -205,7 +205,7 @@ def generate_reports_heatmap(tmpexport, reportsdata, geoNameNNumber, start, end,
             hovertemplate = "<b> %{y}  %{x} <br><br> %{z} % report completeness"
         else:
             hovertemplate = "<b> %{y}  %{x} <br><br> %{text} Reports"  # %{z} Reports"
-
+        
         compcol = [[0, "#FF7777"], [0.2, "#FFAAAA"], [0.4, "#F3D0D7"], [0.6, "#FFE0B5"], [0.8, "#FFF2D7"], [1, "white"]]
         
         data = [
